c8f5fbb3601d17a48ad1d942acd0210bf9e0981c
diptanshumahish <diptanshumahish2016@gmail.com>
2023-09-15 00:52:55 +0530

    correction

--- src/components/Read/ReadWrapper.tsx
+++ src/components/Read/ReadWrapper.tsx
@@ -2,11 +2,19 @@
 import { useGetALlPosts } from "@/api-management/endpoints/posts/query";
 import TextHeading from "@/components/Texts/TextHeading";
 import TrendingCards from "@/components/Trending/TrendingCards";
+import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { BarLoader } from "react-spinners";
 
 export default function ReadWrapper() {
-    const posts = useGetALlPosts();
+    const router = useRouter();
+    const posts = useGetALlPosts({
+        onError: (e) => {
+            console.log(e.message);
+            router.replace("/");
+        },
+    });
 
     return (
         <div className="md:px-[15%] px-[5%]">
